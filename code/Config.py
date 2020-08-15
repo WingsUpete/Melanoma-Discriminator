8d109595b1317cbc9888f90c04a5fd1d8fb314ce
Peter S <petershen815@126.com>
2020-08-15 15:11:03 +0800

    Crop then resize

--- code/Config.py
+++ code/Config.py
@@ -9,14 +9,16 @@ from torchvision import transforms
 # Settings
 def get_train_transform(img_resize=240):
     return transforms.Compose([ \
-        transforms.RandomResizedCrop(size=img_resize, scale=(0.9, 1.0)), \
+        transforms.CenterCrop(512), \
+        transforms.Resize(img_resize), \
         transforms.ToTensor(), \
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # standard for EfficientNet \
         ])
 
 def get_eval_transform(img_resize=240):
     return transforms.Compose([ \
-        transforms.RandomResizedCrop(size=img_resize, scale=(1.0, 1.0)), \
+        transforms.CenterCrop(512), \
+        transforms.Resize(img_resize), \
         transforms.ToTensor(), \
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) \
         ])
