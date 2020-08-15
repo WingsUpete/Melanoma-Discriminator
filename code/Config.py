21a1a998de81ab759b85c325ff6c731d3ca7ef10
Peter S <petershen815@126.com>
2020-08-15 14:06:30 +0800

    make eval transform absolute so every time the same data is scanned

--- code/Config.py
+++ code/Config.py
@@ -16,7 +16,7 @@ def get_train_transform(img_resize=240):
 
 def get_eval_transform(img_resize=240):
     return transforms.Compose([ \
-        transforms.RandomResizedCrop(size=img_resize, scale=(0.996, 1.0)), \
+        transforms.RandomResizedCrop(size=img_resize, scale=(1.0, 1.0)), \
         transforms.ToTensor(), \
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) \
         ])
