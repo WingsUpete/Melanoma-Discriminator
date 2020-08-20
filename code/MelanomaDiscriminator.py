8ebd8eb6a0d20d12778a01fb817e88129f344b5b
Peter S <petershen815@126.com>
2020-08-20 15:19:22 +0800

    finding the bug

--- code/MelanomaDiscriminator.py
+++ code/MelanomaDiscriminator.py
@@ -193,12 +193,13 @@ def eval(model_name, minibatch_size=Config.BATCH_SIZE_DEFAULT, num_workers=Confi
     plt.title('Receiver Operating Characteristic (ROC) of Melanoma Model')
     plt.legend(loc="lower right")
     
-    base = os.path.basename(model_name)
-    filename = os.path.splitext(base)[0]
-    eval_path = os.path.join(Config.EVAL_DEFAULT, filename)
+    eval_base = os.path.basename(model_name)
+    eval_filename = os.path.splitext(eval_base)[0]
+    print(model_name, eval_base, eval_filename)
+    eval_path = os.path.join(Config.EVAL_DEFAULT, eval_filename)
     if not os.path.isdir(eval_path):
         os.mkdir(eval_path)
-    imgname = os.path.join(eval_path, '{}.png'.format(filename))
+    imgname = os.path.join(eval_path, '{}.png'.format(eval_filename))
     plt.savefig(imgname, bbox_inches='tight')
     stdLog(sys.stdout, 'ROC curve saved to {}\n'.format(imgname), DEBUG, fd)
     #plt.show()
