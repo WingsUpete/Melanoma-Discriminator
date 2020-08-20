889170b6e4e62fa41b15281fc39881135907692b
Peter S <petershen815@126.com>
2020-08-20 15:31:39 +0800

    fix directory creation bug

--- code/MelanomaDiscriminator.py
+++ code/MelanomaDiscriminator.py
@@ -196,6 +196,8 @@ def eval(model_name, minibatch_size=Config.BATCH_SIZE_DEFAULT, num_workers=Confi
     eval_base = os.path.basename(model_name)
     eval_filename = os.path.splitext(eval_base)[0]
     eval_path = os.path.join(Config.EVAL_DEFAULT, eval_filename)
+    if not os.path.isdir(Config.EVAL_DEFAULT):
+        os.mkdir(Config.EVAL_DEFAULT)
     if not os.path.isdir(eval_path):
         os.mkdir(eval_path)
     imgname = os.path.join(eval_path, '{}.png'.format(eval_filename))
