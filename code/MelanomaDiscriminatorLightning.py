60a4497ccdd4e00847852a6b263c95759d86b1e3
Peter S <petershen815@126.com>
2020-08-20 12:06:56 +0800

    Refactor to Pytorch Lightning: still in progress

---
+++ code/MelanomaDiscriminatorLightning.py
@@ -0,0 +1,287 @@
+################## Melanoma Discrimator #####################
+###        Created by Peter S on Aug 11th, 2020           ###
+###                petershen815@126.com                   ###
+### Data Source: https://challenge2020.isic-archive.com/  ###
+#############################################################
+# This file serves as the main for the whole module, refactored into
+# the framework PyTorch Lightning: https://github.com/PyTorchLightning/pytorch-lightning
+# More explanation article in Chinese: https://cloud.tencent.com/developer/article/1593703
+# PyTorch's EfficientNet is used, more: https://github.com/lukemelas/EfficientNet-PyTorch#about-efficientnet
+# Pooling and Dropout layers are suggested in https://www.kaggle.com/shaitender/melanoma-efficientnet-pytorch
+
+import os
+import sys
+import argparse
+from datetime import datetime
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+from sklearn.metrics import accuracy_score, roc_auc_score, roc_curve
+
+from efficientnet_pytorch import EfficientNet
+
+import pytorch_lightning as pl
+
+from MelanomaDataSet import MelanomaDataSet
+from MelanomaModel import Net
+
+import Config
+
+def stdLog(stdwhich, str, DEBUG=True, fd=None):
+    """
+    Output to std & log file
+    """
+    if fd:
+        fd.write('{}: {}'.format(datetime.now(), str))
+    if DEBUG:
+        stdwhich.write(str)
+
+class MelanomaDiscriminator(pl.LightningModule):
+
+    def __init__(self, efnet_version=1, rs=Config.RESIZE_DEFAULT, dh=Config.DRAW_HAIR_DEFAULT, minibatch_size=Config.BATCH_SIZE_DEFAULT, \
+                 num_workers=Config.WORKERS_DEFAULT, fd=None, optimizer=Config.OPTIMIZER_DEFAULT):
+        """
+        Initializes a model network.
+        
+        Args:
+            efnet_version (int/str): which version to train on [0, 7]
+        Vars:
+            efnet (EfficientNet): the EfficientNet network to be used
+        """
+        super(MelanomaDiscriminator, self).__init__()
+        
+        if not os.path.isdir(Config.MODEL_DEFAULT):
+            os.mkdir(Config.MODEL_DEFAULT)
+
+        stdLog(sys.stdout, "Loading Melanoma Dataset...\n", DEBUG, fd)
+        self.dataset = MelanomaDataSet(folder, train_transform=Config.get_train_transform(rs, bool(dh)), eval_transform=Config.get_eval_transform(rs))
+        self.batch_size = minibatch_size
+        self.num_workers = num_workers
+        self.optimizer = optimizer
+        self.criterion = nn.BCEWithLogitsLoss()
+
+        self.train_correct = 0
+        self.train_loss = 0
+        self.train_total = len(self.dataset.trainset)
+
+        self.efnet_version = efnet_version if (isinstance(efnet_version, int) and \
+                                               efnet_version in [i + 1 for i in range(7)]) else Config.EFNET_VER_DEFAULT
+        self.efnet = EfficientNet.from_pretrained('efficientnet-b{}'.format(self.efnet_version))
+        in_features = getattr(self.efnet, '_fc').in_features
+        self.drop = nn.Dropout(0.3)
+        self.classifier = nn.Linear(in_features, 1)
+    
+    def forward(self, x):
+        """
+        Performs forward pass of the input.
+        
+        Args:
+          x: input to the network
+        Returns:
+          out: outputs of the network
+        """
+        batch_size = x.shape[0]
+        features = self.efnet.extract_features(x)
+        features = F.adaptive_avg_pool2d(features, 1).reshape(batch_size, -1)
+        dropout = self.drop(features)
+        out = self.classifier(dropout)
+        return out
+
+    def prepare_data(self):
+        """
+        Download and pre-process data. Nothing to do here
+        """
+        pass
+
+    def train_dataloader(self):
+        return DataLoader(self.dataset.trainset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
+
+    def val_dataloader(self):
+        return DataLoader(self.dataset.validset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        return DataLoader(self.dataset.testset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
+
+    def configure_optimizers(self):
+        if self.optimizer == 'ADAM':
+            return torch.optim.Adam(net.parameters(), learning_rate, weight_decay=Config.WEIGHT_DECAY_DEFAULT)    # Adam + L2 Norm
+        elif self.optimizer == 'RMSprop':
+            return torch.optim.RMSprop(net.parameters(), learning_rate, weight_decay=Config.WEIGHT_DECAY_DEFAULT)
+        else:
+            return torch.optim.Adam(net.parameters(), learning_rate, weight_decay=Config.WEIGHT_DECAY_DEFAULT)    # Default: Adam + L2 Norm
+
+    def on_train_start(self, trainer, pl_module):
+        self.train_correct = 0
+        self.train_loss = 0
+
+    def training_step(self, batch, batch_idx):
+        samples, metas, labels = batch['image'], batch['meta'], batch['target']
+        
+        # the following line is to deal with exploding gradients
+        torch.nn.utils.clip_grad_norm_(net.parameters(), max_norm=Config.MAX_NORM_DEFAULT)
+
+        res = self.forward(samples)              # [[1], [2], [3]]
+        loss = self.criterion(res, labels.type_as(res).reshape(-1, 1))  # BCEWithLogitsLoss does not support Long
+        
+        preds = torch.round(torch.sigmoid(res)) # set threshold to be 0.5 so that values below 0.5 will be considered 0
+        self.train_correct += (preds == labels.type_as(res).reshape(-1, 1)).sum().item()
+        self.train_loss += loss.item()
+
+    def on_train_end(self, trainer, pl_module):
+        train_acc = self.train_correct / self.train_total
+        stdLog(sys.stdout, 'Training Round %d: acc = %.2f%%, loss = %.2f\n' % (self.current_epoch, train_acc * 100, ))
+
+
+
+def train(learning_rate=Config.LEARNING_RATE_DEFAULT, minibatch_size=Config.BATCH_SIZE_DEFAULT, ef_ver=Config.EFNET_VER_DEFAULT, \
+          max_epoch=Config.MAX_EPOCHS_DEFAULT,  eval_freq=Config.EVAL_FREQ_DEFAULT, optimizer=Config.OPTIMIZER_DEFAULT, \
+          num_workers=Config.WORKERS_DEFAULT, use_gpu=True, folder=Config.DATA_DIR_DEFAULT, DEBUG=True, fd=None, time_tag='WHEN', \
+          rs=Config.RESIZE_DEFAULT, dh=Config.DRAW_HAIR_DEFAULT):
+    """
+    Performs training and evaluation of the CNN model.
+    """
+
+    # Load Melanoma Datast
+    stdLog(sys.stdout, "Loading Melanoma Dataset...\n", DEBUG, fd)
+    dataset = MelanomaDataSet(folder, train_transform=Config.get_train_transform(rs, bool(dh)), eval_transform=Config.get_eval_transform(rs))
+    trainloader = DataLoader(dataset.trainset, batch_size=minibatch_size, shuffle=True, num_workers=num_workers)
+    validloader = DataLoader(dataset.validset, batch_size=minibatch_size, shuffle=False, num_workers=num_workers)
+    testloader = DataLoader(dataset.testset, batch_size=minibatch_size, shuffle=False, num_workers=num_workers)
+
+    # Initialize the model
+    stdLog(sys.stdout, "Initializing the Training Model...\n", DEBUG, fd)
+    net = Net(efnet_version=ef_ver)
+    criterion = nn.BCEWithLogitsLoss()
+
+    # Select Optimizer
+    if optimizer == 'ADAM':
+        optimizer = torch.optim.Adam(net.parameters(), learning_rate, weight_decay=Config.WEIGHT_DECAY_DEFAULT)    # Adam + L2 Norm
+    elif optimizer == 'RMSprop':
+        optimizer = torch.optim.RMSprop(net.parameters(), learning_rate, weight_decay=Config.WEIGHT_DECAY_DEFAULT)
+    else:
+        optimizer = torch.optim.Adam(net.parameters(), learning_rate, weight_decay=Config.WEIGHT_DECAY_DEFAULT)    # Default: Adam + L2 Norm
+
+    # CUDA if possible
+    device = torch.device("cuda:0" if (bool(use_gpu) and torch.cuda.is_available()) else "cpu")
+    stdLog(sys.stdout, "device: {}\n".format(device), DEBUG, fd)
+
+    if device:
+        net.to(device)
+        stdLog(sys.stdout, "Training Model sent to CUDA\n", DEBUG, fd)
+
+    # Start Training
+    stdLog(sys.stdout, "Start Training!\n", DEBUG, fd)
+    
+    stdLog(sys.stdout, "Using EfficientNet {}, images resized to size = {}\n".format(ef_ver, rs), DEBUG, fd)
+    stdLog(sys.stdout, "learning_rate = {}, max_epoch = {}, num_workers = {}\n".format(learning_rate, max_epoch, num_workers), DEBUG, fd)
+    stdLog(sys.stdout, "eval_freq = {}, minibatch_size = {}, optimizer = {}\n".format(eval_freq, minibatch_size, optimizer), DEBUG, fd)
+    stdLog(sys.stdout, "train_transform = {}, eval_transform = {}\n".format(dataset.train_transform, dataset.eval_transform), DEBUG, fd)
+
+    stdLog(sys.stdout, "------------------------------------------------------------\n", DEBUG, fd)
+    
+    if not os.path.isdir(Config.MODEL_DEFAULT):
+        os.mkdir(Config.MODEL_DEFAULT)
+    
+    best_auc = 0.0
+
+    for epoch_i in range(max_epoch):
+        # train one round
+        net.train()
+        train_correct = 0
+        train_loss = 0
+        for i, batch in enumerate(trainloader):
+            samples, metas, labels = batch['image'], batch['meta'], batch['target']
+            if device:
+                samples, labels = samples.to(device), labels.to(device)
+
+            # the following line is to deal with exploding gradients
+            torch.nn.utils.clip_grad_norm_(net.parameters(), max_norm=Config.MAX_NORM_DEFAULT)
+
+            optimizer.zero_grad()
+            res = net(samples)              # [[1], [2], [3]]
+            loss = criterion(res, labels.type_as(res).reshape(-1, 1))  # BCEWithLogitsLoss does not support Long
+            loss.backward()
+            optimizer.step()
+
+            preds = torch.round(torch.sigmoid(res)) # set threshold to be 0.5 so that values below 0.5 will be considered 0
+            train_correct += (preds == labels.type_as(res).reshape(-1, 1)).sum().item()
+            train_loss += loss.item()
+        train_total = len(dataset.trainset)
+        train_acc = train_correct / train_total
+        stdLog(sys.stdout, 'Training Round %d: acc = %.2f%%, loss = %.2f\n' % (epoch_i, train_acc * 100, loss.item()), DEBUG, fd)
+    
+        # evaluate every eval_freq
+        if ((epoch_i + 1) % eval_freq == 0):
+            net.eval()
+            with torch.no_grad():
+                val_pred_list = torch.zeros((len(dataset.validset), 1)).to(device)
+                # Evaluate using the validation set
+                for j, val_batch in enumerate(validloader):
+                    val_samples, val_metas, val_labels = val_batch['image'], val_batch['meta'], val_batch['target']
+                    if device:
+                        val_samples, val_labels = val_samples.to(device), val_labels.to(device)
+                    val_res = net(val_samples)
+                    val_pred = torch.sigmoid(val_res)
+                    val_pred_list[j * validloader.batch_size : j * validloader.batch_size + len(val_samples)] = val_pred
+                val_label_list = dataset.validset.label_list.type_as(val_pred_list).reshape(-1, 1)
+                val_acc = accuracy_score(val_label_list.cpu(), torch.round(val_pred_list.cpu()))    # accuracy on threshold value = 0.5
+                val_roc_auc = roc_auc_score(val_label_list.cpu(), val_pred_list.cpu())               # AUC score
+                stdLog(sys.stdout, '!!! Validation : acc = %.2f%%, roc_auc = %.2f%% !!!\n' % (val_acc * 100, val_roc_auc * 100), DEBUG, fd)
+
+                if train_acc >= 0.9 and val_roc_auc > best_auc:
+                    best_auc = val_roc_auc
+                    model_name = os.path.join(Config.MODEL_DEFAULT, '{}.pth'.format(time_tag))
+                    torch.save(net, model_name)
+                    stdLog(sys.stdout, 'Model: {} has been saved.\n'.format(model_name), DEBUG, fd)
+    
+    #net = torch.load(model_name)
+
+if __name__ == '__main__':
+    # Command Line Arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-lr', '--learning_rate', type = float, default = Config.LEARNING_RATE_DEFAULT, \
+                        help='Learning rate, default = {}'.format(Config.LEARNING_RATE_DEFAULT))
+    parser.add_argument('-me', '--max_steps', type = int, default = Config.MAX_EPOCHS_DEFAULT, \
+                        help='Number of epochs to run the trainer, default = {}'.format(Config.MAX_EPOCHS_DEFAULT))
+    parser.add_argument('-ef', '--eval_freq', type = int, default = Config.EVAL_FREQ_DEFAULT, \
+                        help='Frequency of evaluation on the validation set, default = {}'.format(Config.EVAL_FREQ_DEFAULT))
+    parser.add_argument('-bs', '--minibatch_size', type = int, default = Config.BATCH_SIZE_DEFAULT, \
+                        help='Size of a minibatch, default = {}'.format(Config.BATCH_SIZE_DEFAULT))
+    parser.add_argument('-opt', '--optimizer', type = str, default = Config.OPTIMIZER_DEFAULT, \
+                        help='Optimizer to be used [Adam, RMSprop], default = {}'.format(Config.OPTIMIZER_DEFAULT))
+    parser.add_argument('-dr', '--data_dir', type = str, default = Config.DATA_DIR_DEFAULT, \
+                        help='Root directory of the input data, default = {}'.format(Config.DATA_DIR_DEFAULT))
+    parser.add_argument('-log', '--log', type = str, default = Config.LOG_DEFAULT, \
+                        help='Specify where to create a log file. If log files are not wanted, value will be None'.format(Config.LOG_DEFAULT))
+    parser.add_argument('-c', '--cores', type=int, default=Config.WORKERS_DEFAULT, \
+                        help='number of workers (cores used), default = {}'.format(Config.WORKERS_DEFAULT))
+    parser.add_argument('-gpu', '--gpu', type=int, default=Config.USE_GPU_DEFAULT, \
+                        help='Specify whether to use GPU, default = {}'.format(Config.USE_GPU_DEFAULT))
+    parser.add_argument('-ev', '--efnet_version', type=int, default=Config.EFNET_VER_DEFAULT, \
+                        help='The version of EfficientNet to be used, default = {}'.format(Config.EFNET_VER_DEFAULT))
+    parser.add_argument('-rs', '--resize', type=int, default=Config.RESIZE_DEFAULT, \
+                        help='The resized size of image, default = {}'.format(Config.RESIZE_DEFAULT))
+    parser.add_argument('-dh', '--draw_hair', type=int, default=Config.DRAW_HAIR_DEFAULT, \
+                        help='Specify whether to draw pseudo-hairs in images, default = {}'.format(Config.DRAW_HAIR_DEFAULT))
+
+    FLAGS, unparsed = parser.parse_known_args()
+
+    time_tag = datetime.now().strftime('%Y%m%d_%H_%M_%S')
+
+    # Starts a log file in the specified directory
+    if FLAGS.log:
+        if not os.path.isdir(FLAGS.log):
+            os.mkdir(FLAGS.log)
+        fd = open(os.path.join(FLAGS.log, '{}.log'.format(time_tag)), 'w')
+    else:
+        fd = None
+
+    train(learning_rate = FLAGS.learning_rate, minibatch_size = FLAGS.minibatch_size, max_epoch = FLAGS.max_steps, \
+          ef_ver=FLAGS.efnet_version, eval_freq = FLAGS.eval_freq, optimizer = FLAGS.optimizer, num_workers=FLAGS.cores, \
+          use_gpu = FLAGS.gpu, folder = FLAGS.data_dir, DEBUG = True, fd = fd, time_tag=time_tag, rs = FLAGS.resize, dh = FLAGS.draw_hair)
